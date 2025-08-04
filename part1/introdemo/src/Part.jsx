b2e562b847c260a7319678122e9377661329a4a4
j0th4r <124442886+j0th4r@users.noreply.github.com>
2025-08-04 14:54:33 +0800

    Submit part1

--- part1/introdemo/src/Part.jsx
+++
@@ -1,6 +0,0 @@
-function Part(props) {
-  return (
-    <p>{props.name} {props.exercises}</p>
-  )
-}
-export default Part
--- part1/introdemo/src/data.js
+++
@@ -1,7 +0,0 @@
-export const COUNTRIES = {
-  US: "United States",
-  GB: "United Kingdom",
-  CA: "Canada",
-  IN: "India",
-  AU: "Australia",
-};
