307ff1ec9ecb75dbec3317715cb3893fd49fcaa1
j0th4r <124442886+j0th4r@users.noreply.github.com>
2025-10-11 23:36:03 +0800

    Finish exercise 3.9 Phonebook backend step 9

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-app.use(express.json());
+const cors = require('cors');
 
+app.use(cors());
+app.use(express.json());
 
 morgan.token('body', function (req, res) {
   return JSON.stringify(req.body);
@@ -102,7 +104,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
