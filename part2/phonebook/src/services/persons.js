3c699dc410d1f184c4eef74d1be7f4781b9852cd
j0th4r <124442886+j0th4r@users.noreply.github.com>
2025-10-12 00:05:09 +0800

    Finish exercise 3.10 Phonebook backend step 10

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://phonebook-78is.onrender.com/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
