31109d0889ae14c779e702492de96545eff9793f
j0th4r <124442886+j0th4r@users.noreply.github.com>
2025-10-13 21:32:42 +0800

    Finish exercise 3.22: Lint configuration

--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -1,45 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('give password as argument');
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
-const name = process.argv[3];
-const number = process.argv[4];
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.xaw8lbt.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`;
+const url = `mongodb+srv://fullstack:${password}@cluster0.xaw8lbt.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 
-mongoose.connect(url);
+mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
-});
+})
 
-const Person = mongoose.model('Person', personSchema);
+const Person = mongoose.model('Person', personSchema)
 
 if (name && number) {
   const person = new Person({
     name,
     number
-  });
+  })
 
   person.save().then((result) => {
-    console.log(`added ${name} number ${number} to phonebook`);
-    mongoose.connection.close();
-  });
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
 }
 
 if (!name && !number) {
   Person.find({}).then((result) => {
     console.log('phonebook:')
     result.forEach((persons) => {
-      console.log(`${persons.name} ${persons.number}`);
-    });
-    mongoose.connection.close();
-  });
+      console.log(`${persons.name} ${persons.number}`)
+    })
+    mongoose.connection.close()
+  })
 }
