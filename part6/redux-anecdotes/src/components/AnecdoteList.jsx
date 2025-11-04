0b65c8cf2e2befea1f9257eecacfae8c0ca4d498
j0th4r <124442886+j0th4r@users.noreply.github.com>
2025-11-04 21:37:48 +0800

    Finish exercise 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,9 +2,13 @@ import { useSelector, useDispatch } from 'react-redux';
 import { addVote } from '../reducers/anecdoteReducer';
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) =>
-    [...state].sort((a, b) => b.votes - a.votes)
-  );
+  const anecdotes = useSelector(({ filter, anecdotes }) => {
+    const filtered = anecdotes.filter((anecdote) =>
+      anecdote.content.toLowerCase().search(filter.toLowerCase()) > -1
+    );
+
+    return filtered.sort((a, b) => b.votes - a.votes);
+  });
   const dispatch = useDispatch();
 
   const vote = (id) => {
--- part6/redux-anecdotes/src/components/Filter.jsx
+++ part6/redux-anecdotes/src/components/Filter.jsx
@@ -1,8 +1,13 @@
+import { useDispatch } from "react-redux";
+import { filterChange } from "../reducers/filterReducer";
+
 const Filter = () => {
+  const dispatch = useDispatch();
+
   const handleChange = (event) => {
-    // input-field value is in variable event.target.value
     const value = event.target.value
-    console.log(value  )
+  
+    dispatch(filterChange(value))
   };
   const style = {
     marginBottom: 10,
