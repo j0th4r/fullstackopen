f6144272c5329da73f993877c742bb4539944232
j0th4r <124442886+j0th4r@users.noreply.github.com>
2025-11-05 17:21:18 +0800

    Finish exercises 6.10-6.11

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 const anecdotesAtStart = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
@@ -19,42 +21,25 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject);
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'NEW_ANECDOTE':
-      return [...state, action.payload];
-    case 'ADD_VOTE': {
-      const id = action.payload.id;
-      const anecdote = state.find((n) => n.id === id);
-      const nextAnecdote = {
-        ...anecdote,
-        votes: anecdote.votes + 1,
-      };
-      return state.map((anecdote) =>
-        anecdote.id !== id ? anecdote : nextAnecdote
-      );
-    }
-    default:
-      return state;
-  }
-};
-
-export const addVote = (id) => {
-  return {
-    type: 'ADD_VOTE',
-    payload: { id },
-  };
-};
-
-export const createAnecdote = (content) => {
-  return {
-    type: 'NEW_ANECDOTE',
-    payload: {
-      content,
-      id: getId(),
-      votes: 0,
+const anecdoteSlice = createSlice({
+  name: 'anecdotes',
+  initialState,
+  reducers: {
+    createAnecdote(state, action) {
+      const content = action.payload
+      state.push({
+        content,
+        id: getId(),
+        votes: 0,
+      });
     },
-  };
-};
+    addVote(state, action) {
+      const id = action.payload
+      const anecdote = state.find((n) => n.id === id);
+      anecdote.votes += 1
+    }
+  }
+})
 
-export default reducer;
+export const { addVote, createAnecdote } = anecdoteSlice.actions
+export default anecdoteSlice.reducer;
