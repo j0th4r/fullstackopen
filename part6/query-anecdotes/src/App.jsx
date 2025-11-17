2c2a3b022dad524129dccd99245e00448ba410c3
j0th4r <124442886+j0th4r@users.noreply.github.com>
2025-11-17 22:36:46 +0800

    fix: install missing tanstack library

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,6 +1,6 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { useQuery, useMutation, useQueryClient } from '@'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import { getAnecdotes, updateAnecdote } from './requests'
 
 const App = () => {
