c505eb53d72a0c2abf3afe260817ceec8fd5f7e9
j0th4r <124442886+j0th4r@users.noreply.github.com>
2025-12-12 17:03:15 +0800

    Finish exercise 8.11

--- part8/library-frontend/src/components/Authors.jsx
+++ part8/library-frontend/src/components/Authors.jsx
@@ -1,13 +1,60 @@
-import { useQuery } from "@apollo/client/react";
-import { ALL_AUTHORS } from "../queries";
+import { useMutation, useQuery } from '@apollo/client/react';
+import { ALL_AUTHORS, UPDATE_AUTHOR } from '../queries';
+import { useState } from 'react';
+
+const BirthYearForm = () => {
+  const [name, setName] = useState('');
+  const [birthYear, setBirthYear] = useState('');
+
+  const [updateAuthor] = useMutation(UPDATE_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  });
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    console.log('update birth year...');
+    console.log(name);
+    console.log(birthYear);
+    //updateAuthor mutation to set or upodate author's birth year
+    updateAuthor({ variables: { name, setBornTo: birthYear } });
+
+    setName('');
+    setBirthYear('');
+  };
+
+  return (
+    <div>
+      <h2>Set birthyear</h2>
+      <form onSubmit={submitHandler}>
+        <div>
+          name
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          born
+          <input
+            type="number"
+            value={birthYear}
+            onChange={({ target }) => setBirthYear(Number(target.value))}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
+    </div>
+  );
+};
 
 const Authors = ({ show }) => {
-  const { data: authorsData, loading } = useQuery(ALL_AUTHORS)
-  
-  if(loading) {
-    return <div>loading...</div>
+  const { data: authorsData, loading } = useQuery(ALL_AUTHORS);
+
+  if (loading) {
+    return <div>loading...</div>;
   }
-  
+
   if (!show) {
     return null;
   }
@@ -31,6 +78,8 @@ const Authors = ({ show }) => {
           ))}
         </tbody>
       </table>
+
+      <BirthYearForm />
     </div>
   );
 };
