7d79ca3df2cb04b1c8c9114f128e0c118c444d31
j0th4r <124442886+j0th4r@users.noreply.github.com>
2026-01-06 14:11:28 +0800

    Implement diary creation with validation

--- part9/flight-diary/src/routes/diaries.ts
+++ part9/flight-diary/src/routes/diaries.ts
@@ -1,15 +1,40 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express, { Response } from 'express';
 import diaryService from '../services/diaryService';
 import { NonSensitiveDiaryEntry } from '../types';
+import toNewDiaryEntry from '../utils';
 
 const router = express.Router();
 
 router.get('/', (_req, res: Response<NonSensitiveDiaryEntry[]>) => {
   res.send(diaryService.getNonSensitiveEntries());
+  return;
 });
 
-router.post('/', (_req, res) => {
-  res.send('Saving a diary!');
+router.get('/:id', (req, res: Response<NonSensitiveDiaryEntry>) => {
+  const diary = diaryService.findById(Number(req.params.id));
+  if (!diary) {
+    res.sendStatus(404);
+    return;
+  }
+  res.send(diary);
+  return;
+});
+
+router.post('/', (req, res) => {
+  try {
+    const newDiaryEntry = toNewDiaryEntry(req.body);
+    const addedEntry = diaryService.addDiary(newDiaryEntry);
+    res.json(addedEntry);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += 'Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
+
+  return;
 });
 
 export default router;
--- part9/flight-diary/src/services/diaryService.ts
+++ part9/flight-diary/src/services/diaryService.ts
@@ -1,6 +1,5 @@
 import diaries from '../../data/entries';
-
-import { NonSensitiveDiaryEntry, DiaryEntry } from '../types';
+import { NewDiaryEntry, NonSensitiveDiaryEntry, DiaryEntry } from '../types';
 
 const getEntries = (): DiaryEntry[] => {
   return diaries;
@@ -10,12 +9,24 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
   return diaries.map(({ id, date, weather, visibility }) => ({ id, date, weather, visibility }));
 };
 
-const addDiary = () => {
-  return null;
+const addDiary = (entry: NewDiaryEntry): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map((d) => d.id)) + 1,
+    ...entry,
+  };
+
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
+};
+
+const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find((d) => d.id === id);
+  return entry;
 };
 
 export default {
   getEntries,
   addDiary,
   getNonSensitiveEntries,
+  findById,
 };
