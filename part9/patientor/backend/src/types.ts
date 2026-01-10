4c890c6cbc3701a5a0e95da00a150d33835b9f30
j0th4r <124442886+j0th4r@users.noreply.github.com>
2026-01-10 15:52:15 +0800

    Finish exercise 9.23

--- part9/patientor/backend/src/types.ts
+++ part9/patientor/backend/src/types.ts
@@ -13,7 +13,48 @@ export interface Diagnosis {
   latin?: string;
 }
 
-export interface Entry {}
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+type SickLeave = {
+  startDate: string;
+  endDate: string;
+};
+
+type Discharge = {
+  date: string;
+  criteria: string;
+};
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: Discharge;
+}
+
+export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
 
 export type PublicPatient = Omit<Patient, 'ssn'>;
 
@@ -23,4 +64,5 @@ export type NewPatient = z.infer<typeof NewPatientSchema>;
 
 export interface Patient extends NewPatient {
   id: string;
+  entries: Entry[];
 }
