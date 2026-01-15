f8cf0c9c23a33067ec5ac20ec5b2fa1c7411792c
j0th4r <124442886+j0th4r@users.noreply.github.com>
2026-01-15 19:34:42 +0800

    Finish exercise 10.3

--- part10/rate-repository-app/App.js
+++ part10/rate-repository-app/App.js
@@ -1,25 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './src/components/Main';
 
-const colors = {
-  white: '#FFFFFF', 
+const App = () => {
+  return <Main />;
 };
 
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: colors.white,
-    flex: 1,
-    justifyContent: 'center',
-  },
-});
+export default App;
