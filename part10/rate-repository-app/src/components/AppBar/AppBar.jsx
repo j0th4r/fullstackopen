ec588967ac9fdf78367b7538520f9a9bfdd595f0
j0th4r <124442886+j0th4r@users.noreply.github.com>
2026-01-17 14:20:03 +0800

    fix: appbar component is using sign in code

--- part10/rate-repository-app/src/components/AppBar/AppBar.jsx
+++ part10/rate-repository-app/src/components/AppBar/AppBar.jsx
@@ -1,66 +1,23 @@
-import { Pressable, StyleSheet, TextInput, View } from 'react-native';
-import Text from './Text';
-import { useFormik } from 'formik';
-import theme from '../theme';
-
-const SignIn = () => {
-  const initialValues = {
-    username: '',
-    password: '',
-  };
-
-  const onSubmit = (values) => {
-    console.log(values);
-  };
-
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-  });
-
-  return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        value={formik.values.username}
-        onChangeText={formik.handleChange('username')}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        value={formik.values.height}
-        onChangeText={formik.handleChange('password')}
-        secureTextEntry
-      />
-      <Pressable style={styles.submitBtn} onPress={formik.handleSubmit}>
-        <Text fontSize="subheading" fontWeight="bold" color="textBar">
-          Sign in
-        </Text>
-      </Pressable>
-    </View>
-  );
-};
-
-export default SignIn;
+import { StyleSheet, ScrollView } from 'react-native';
+import Constants from 'expo-constants';
+import theme from '../../theme';
+import AppBarTab from './AppBarTab';
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: theme.colors.formBg,
-    padding: 16,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#B8B8B8',
-    borderRadius: 4,
-    padding: 16,
-    fontSize: theme.fontSizes.subheading,
-    marginBottom: 16,
-  },
-  submitBtn: {
-    backgroundColor: theme.colors.primary,
-    borderRadius: 4,
-    padding: 16,
-    justifyContent: 'center',
+    backgroundColor: theme.colors.appBarBackground,
+    paddingTop: Constants.statusBarHeight,
+    flexGrow: 0,
   },
 });
+
+const AppBar = () => {
+  return (
+    <ScrollView style={styles.container} horizontal>
+      <AppBarTab route="/">Repositories</AppBarTab>
+      <AppBarTab route="/signin">Sign In</AppBarTab>
+    </ScrollView>
+  );
+};
+
+export default AppBar;
