8fee48446edc16430cc07822cbe3e400b76f638b
j0th4r <124442886+j0th4r@users.noreply.github.com>
2026-01-19 09:44:51 +0800

    Finish exercise 10.13

--- part10/rate-repository-app/src/components/SignIn.jsx
+++ part10/rate-repository-app/src/components/SignIn.jsx
@@ -4,8 +4,11 @@ import TextInput from './TextInput';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 import theme from '../theme';
+import useSignIn from '../hooks/useSignIn';
 
 const SignIn = () => {
+  const [signIn] = useSignIn();
+
   const initialValues = {
     username: '',
     password: '',
@@ -16,8 +19,15 @@ const SignIn = () => {
     password: yup.string().required('Password is required'),
   });
 
-  const onSubmit = (values) => {
-    console.log(values);
+  const onSubmit = async (values) => {
+    const { username, password } = values;
+
+    try {
+      const { data } = await signIn({ username, password });
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   const formik = useFormik({
